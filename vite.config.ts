dad63fc4561d25d201ffbad4df10dde9a2e63758
asper <aspergillusplay@gmail.com>
2024-10-22 16:44:54 +0200

    Made this project PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      outDir: 'build',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png', 'assets/images/*'],
+      manifest: {
+        short_name: 'radio-app',
+        name: 'My Progressive Web Radio App',
+        icons: [
+          {
+            src: 'assets/images/android-icon-192x192.png',
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: 'assets/images/apple-icon.png',
+            type: 'image/png',
+            sizes: '512x512'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      }
+    })
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
+});
---
+++ workbox-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: [
+    '**/*.{html,js,css,png,jpg,svg,json,tsx,ts,jsx}',
+  ],
+  globIgnores: [
+    'sw.js',
+  ],
+  swDest: 'build/sw.js',
+}
